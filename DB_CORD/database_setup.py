50c1f6aac3cfc064fd3cca5b2854d7cd0b6a4b5d
rainnn99 <rainnnpark99@gmail.com>
2023-04-19 14:30:45 +0900

    DB_setup_test

---
+++ DB_CORD/database_setup.py
@@ -0,0 +1,28 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="test",
+  password="test"
+)
+
+mycursor = mydb.cursor()
+mycursor.execute("USE testdb")
+
+#mycursor.execute('CREATE TABLE customers (test1 INT AUTO_INCREMENT PRIMARY KEY, test2 VARCHAR(255))')
+
+sql = "INSERT INTO customers (test2) VALUES (%s)"
+val = ("John",)
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print("1 record inserted, ID:", mycursor.lastrowid)
+
+mycursor.close()
+mydb.close()
+
+
+
+
+
