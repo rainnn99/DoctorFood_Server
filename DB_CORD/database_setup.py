23483332d74edc899da6e83052c5623a744ee794
ParkJongHyeok99 <jh0310p@naver.com>
2023-05-01 21:00:19 +0900

    test

--- DB_CORD/database_setup.py
+++ DB_CORD/database_setup.py
@@ -1,15 +1,15 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="test",
-  password="test"
+    host="localhost",
+    user="test",
+    password="test"
 )
 
 mycursor = mydb.cursor()
 mycursor.execute("USE testdb")
 
-#mycursor.execute('CREATE TABLE customers (test1 INT AUTO_INCREMENT PRIMARY KEY, test2 VARCHAR(255))')
+# mycursor.execute('CREATE TABLE customers (test1 INT AUTO_INCREMENT PRIMARY KEY, test2 VARCHAR(255))')
 
 sql = "INSERT INTO customers (test2) VALUES (%s)"
 val = ("John",)
@@ -21,5 +21,3 @@ print("1 record inserted, ID:", mycursor.lastrowid)
 
 mycursor.close()
 mydb.close()
-
-#test
