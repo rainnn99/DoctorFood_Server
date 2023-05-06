eefa28107d2b15c85e9272ec4b06c1e732281051
ParkJongHyeok99 <jh0310p@naver.com>
2023-05-06 22:35:37 +0900

    Update database_val_return.py
    user, database 수정

--- DB_CORD/database_val_return.py
+++ DB_CORD/database_val_return.py
@@ -1,10 +1,28 @@
+# import mysql.connector
+
+# mydb = mysql.connector.connect(
+#    host="localhost",
+#    user="test",
+#    password="test",
+#    database="testdb"
+# )
+
+# mycursor = mydb.cursor()
+
+# mycursor.execute("SELECT test2 FROM customers WHERE test1 = 2")
+
+# result = mycursor.fetchone()
+
+# print(result[0])
+
+
 import mysql.connector
 
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="test",
-  password="test",
-  database="testdb"
+    host="localhost",
+    user="root",
+    password="",  # 비밀번호 입력
+    database="food_doctor"
 )
 
 mycursor = mydb.cursor()
