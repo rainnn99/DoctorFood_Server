857f028f90594b83720f73a22236b923ca0d9126
ParkJongHyeok99 <jh0310p@naver.com>
2023-05-08 20:40:57 +0900

    webserver test

--- WebServer/app.py
+++ WebServer/app.py
@@ -1,22 +1,18 @@
 # pip install Flask
 
 from flask import Flask, render_template
+import sys
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-    return 'hello'
-
-
-@app.route('/test')
 def test():
     return render_template("test.html")
 
 
 def main():
-    app.run(host='127.0.0.1', debug=False, port=80)
+    app.run(host='0.0.0.0', port=80)
 
 
 if __name__ == "__main__":
