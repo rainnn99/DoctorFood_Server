4b6abb1966265be8e8ab0fc7e6fc2c332ab89da6
ParkJongHyeok99 <jh0310p@naver.com>
2023-05-09 22:58:16 +0900

    로그인 로그아웃 기능 추가 및 포트번호 변경

--- WebServer/app.py
+++ WebServer/app.py
@@ -1,19 +1,53 @@
 # pip install Flask
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, session, request, redirect
 import sys
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def test():
-    return render_template("test.html")
+app.secret_key = "lfko2dfk5-!fgkfiapvn4"
 
 
 def main():
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
 
 
 if __name__ == "__main__":
     main()
+
+
+ID = 123  # DB 에서 불러오기
+PW = 123  # DB 에서 불러오기
+
+# 홈화면
+
+
+@app.route('/')
+def home():
+    if 'id' in session:
+        return render_template('home.html', login=True)
+    else:
+        return render_template("home.html", login=False)
+
+# 로그인
+
+
+@app.route('/login', methods=["post"])
+def login():
+    global ID, PW
+    _id_ = request.args.get("id")
+    _password_ = request.args.get("pw")
+
+    for i in ID:
+        if _id_ == ID and _password_ == PW:
+            session['id'] = _id_
+            return redirect(url_for("home"))
+        else:
+            return redirect(url_for("home"))
+
+# 로그아웃
+
+
+@app.route('/logout')
+def logout():
+    session.pop("id")
+    return redirect(url_for("home"))
