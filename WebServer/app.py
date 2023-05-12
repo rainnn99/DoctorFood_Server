8bc0efe17eaf33aba66ff078c645612539e18ede
ParkJongHyeok99 <jh0310p@naver.com>
2023-05-12 22:29:01 +0900

    음식추천, 커뮤니티, 회원가입 기능 추가

--- WebServer/app.py
+++ WebServer/app.py
@@ -2,9 +2,17 @@
 
 from flask import Flask, render_template, url_for, session, request, redirect
 import sys
+import pymysql
 
 app = Flask(__name__)
 app.secret_key = "lfko2dfk5-!fgkfiapvn4"
+
+
+conn = pymysql.connect(host='localhost',
+                       user='root',
+                       password='',  # 비밀번호
+                       db='food_recommendation',
+                       charset='utf8')
 
 
 def main():
@@ -15,12 +23,7 @@ if __name__ == "__main__":
     main()
 
 
-ID = 123  # DB 에서 불러오기
-PW = 123  # DB 에서 불러오기
-
-# 홈화면
-
-
+#  홈화면
 @app.route('/')
 def home():
     if 'id' in session:
@@ -28,21 +31,30 @@ def home():
     else:
         return render_template("home.html", login=False)
 
+
 # 로그인
+# @app.route('/login')
+# def login():
+#    return render_template('login.html')
 
-
+# 로그인
+# 아이디 비밀번호 확인
 @app.route('/login', methods=["post"])
 def login():
-    global ID, PW
-    _id_ = request.args.get("id")
-    _password_ = request.args.get("pw")
+    id = request.args.get("id")
+    password = request.args.get("password")
 
-    for i in ID:
-        if _id_ == ID and _password_ == PW:
-            session['id'] = _id_
-            return redirect(url_for("home"))
-        else:
-            return redirect(url_for("home"))
+    with conn:
+        with conn.cursor() as cursor:
+            cursor.execute(
+                'SELECT * FROM customer WHERE username = %s AND password = %s', (id, password))
+            user = cursor.fetchone()
+
+    if user:
+        session['id'] = user[1]
+        return redirect(url_for("home"))
+    else:
+        return redirect(url_for("home"))
 
 # 로그아웃
 
@@ -50,4 +62,88 @@ def login():
 @app.route('/logout')
 def logout():
     session.pop("id")
-    return redirect(url_for("home"))
+    return render_template('home.html')
+
+
+# 회원가입
+@app.route('/signup', methods=['POST'])
+def sign_up():
+    id = request.args.get("id")
+    password = request.args.get("password")
+    name = request.args.get("name")
+    birth = request.args.get("birth")
+    sex = request.args.get("sex")
+    phone_number = request.args.get("phone_number")
+    email = request.args.get("eamil")
+    job = request.args.get("job")
+    height = request.args.get("height")
+    weight = request.args.get("weight")
+    bmi = request.args.get("bmi")
+
+    sql = "INSERT INTO customer (id, password, name, birth, sex, phone_number, email, job, height, weight, bmi) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+    with conn:
+        with conn.cursor() as cur:
+            cur.execute(sql, (id, password, name, birth, sex,
+                        phone_number, email, job, height, weight, bmi))
+            conn.commit()
+
+    return redirect(url_for("login"))
+
+
+# 커뮤니티_글작성
+@app.route('/community_writing', methods=['POST'])
+def community_writing():
+    id = request.args.get("id")
+    title = request.args.get("title")
+    main_text = request.args.get("main_text")
+
+    sql = "INSERT INTO community (id, title, main_text) VALUES (%s, %s, %s)"
+
+    with conn:
+        with conn.cursor() as cur:
+            cur.execute(sql, (id, title, main_text))
+            conn.commit()
+
+    return redirect(url_for("community_list"))
+
+
+# 커뮤니티_글목록
+@app.route('/community_list', methods=['POST'])
+def community_list():
+
+    sql = "select * from community"
+
+    with conn:
+        with conn.cursor() as cur:
+            cur.execute(sql)
+
+    row = cur.fetchall()
+
+    return render_template('community.html', row=row)
+
+# 음식추천
+
+
+@app.route('/food_recommendation', methods=['POST'])
+def food_recommendation():
+    sql_preferred_food = "select * from preferred_food"
+
+    with conn:
+        with conn.cursor() as cur:
+            cur.execute(sql_preferred_food)
+
+    preferred_food = cur.fetchall()
+
+    sql_healthy_food = "select * from healthy_food"
+
+    with conn:
+        with conn.cursor() as cur:
+            cur.execute(sql_healthy_food)
+
+    healthy_food = cur.fetchall()
+
+    return render_template('recommendation.html', preferred_food=preferred_food, healthy_food=healthy_food)
+
+
+# 게임
